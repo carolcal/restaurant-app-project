55d1b0e1616b15cb147e9fbfe61be133fce4b8b4
Caroline Caleguer <carolineyamashitac@gmail.com>
2023-05-22 17:15:09 -0300

    MenuItem, NavBar, SubHeading, AboutUs, Chef, Gallery, Header, Intro, Laurels, SpecialMenu

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+import { images } from '../../constants'
+import { SubHeading } from '../../components';
 
 import './Gallery.css';
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
 
 export default Gallery;
